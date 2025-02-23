407a9731d464a1d93a4fb54488464ce172d47202
TRANNHATistar <trannhat0698@gmail.com>
2025-02-23 09:54:30 +0700

    the king

---
+++ script tsb.lua
@@ -0,0 +1,231 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+local Window = MakeWindow({
+    Hub = {
+        Title = "Cat",
+        Animation = "Youtube: @Nhattran0698"
+    },
+    Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+            Notifications = true,
+            CorrectKey = "Running the Script...",
+            Incorrectkey = "The key is incorrect",
+            CopyKeyLink = "Copied to Clipboard"
+        }
+    }
+})
+
+MinimizeButton({
+    Image = "http://www.roblox.com/asset/?id=86147935857683", -- URL hình ảnh đã được thay đổi
+    Size = {33, 33},
+    Color = Color3.fromRGB(10, 10, 10),
+    Corner = true,
+    Stroke = false,
+    StrokeColor = Color3.fromRGB(255, 0, 0)
+})
+
+-- Tab
+local Tab1o = MakeTab({Name = "Script to my friends"})
+local Tab2o = MakeTab({Name = "Script vip vãz nho"})
+
+-- Button
+AddButton(Tab1o, {
+    Name = "Hỏa quyền fifai(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/EMWSLp9A"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "JK(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/UltimateKJPlayer/JK-MOVESET-V1.0/refs/heads/main/JK%20MOVESET%20V1.0%20(1).txt"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Gộ v1(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SerialdesignationV-collab/Gojo-with-no-speech-when-executed/refs/heads/main/I'm%20pregnant.txt"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Gộ v2(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/777Q4QDN",true))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Sú kuto(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/damir512/sukunasaitamav1/main/thescript",true))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "God of war(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/4r1z0na/TSB/refs/heads/main/Kratos/Kratos.luau"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Dark lord(xài samurai)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Qaiddanial2904/Main/refs/heads/main/Dark%20lord"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Gộ v3(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SerialdesignationV-collab/Gojo-with-no-speech-when-executed/refs/heads/main/I'm%20pregnant.txt"))()
+    end
+})
+
+AddButton(Tab1o, {
+    Name = "Goku(xài garuo)",
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/The-Strongest-Battlegrounds-Hero-hunter-moveset-into-OP-goku-moveset-17468"))()
+    end
+})
+AddButton(Tab1o, {
+    Name = "Naruto(xài saitama)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LolnotaKid/NarutoBeatUpSasukeAss/refs/heads/main/NarutoCums"))()
+  end
+})
+AddButton(Tab1o, {
+    Name = "Deku(xài shỉu)",
+    Callback = function()
+       loadstring(game:HttpGet("https://github.com/aggiealledge/obfuscated-scripts/raw/refs/heads/main/deku%20suiryu%20thingy.txt"))()
+  end
+})
+AddButton(Tab1o, {
+    Name = "Quỷ cưa(xài ninja)",
+    Callback = function()
+      loadstring(game:HttpGet("https://gist.githubusercontent.com/GoldenHeads2/0fd8d36993c850f3fac89e5adf793076/raw/ab4f5a42bd0b2e24a32a46301d533ea849ca771c/gistfile1.txt"))()
+  end
+})
+AddButton(Tab1o, {
+    Name = "Luffy(xài saitama)",
+    Callback = function()
+     loadstring(game:HttpGet("https://github.com/aggiealledge/obfuscated-scripts/raw/refs/heads/main/Protected_7732857839120517.txt"))()
+  end
+})
+AddButton(Tab2o, {
+    Name = "Otaku(xài gorou)",
+    Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Kenjihin69/Kenjihin69/refs/heads/main/Hero%20hunter%20to%20okarun%20fr"))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "An rồ(xài gorou)",
+    Callback = function()
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/GoldenHeads2/93ddb1b1e2935bc76fbc37aae8985ad2/raw/47dfa45e1e8f1933a1c031b198d40c3cd1499fa2/gistfile1.txt"))() 
+ end
+})
+AddButton(Tab2o, {
+    Name = "Alaska(xài gorou)",
+    Callback = function()
+    loadstring(game:HttpGet("https://paste.ee/r/zzvAH"))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "ErEreen mất đàu(xài shift)",
+    Callback = function()
+    loadstring(game:HttpGet("https://paste.ee/r/fxY03"))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "haha scam(xài ga tàu)",
+    Callback = function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/eEDYWj8p"))()
+ end
+})
+AddButton(Tab1o, {
+    Name = "Toji(xài Samurai)",
+    Callback = function()
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/Wi-sp/Limitless-legacy/refs/heads/main/GUI")()
+ end
+})
+AddButton(Tab1o, {
+    Name = "Chara(xài saitama)",
+    Callback = function()
+  loadstring(game:HttpGet("https://paste.ee/r/0yYkO"))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "Hakari(xài saitama)",
+    Callback = function()
+  loadstring(game:HttpGet("https://pastebin.com/raw/eEDYWj8p"))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "Otakun(xài garou)",
+    Callback = function()
+  loadstring(game:HttpGet"https://paste.ee/r/Pn4oj")()
+ end
+})
+AddButton(Tab1o, {
+    Name = "Sukuna(xài saitama)",
+    Callback = function()
+ loadstring(game:HttpGet('https://pastebin.com/raw/rL3hyRQV'))()
+ end
+})
+AddButton(Tab2o, {
+    Name = "Humbleds(xài garou)",
+    Callback = function()
+ loadstring(game:HttpGet("https://paste.ee/r/7FuWC"))()
+ end
+})
+AddButton(Tab1o, {
+    Name = "Jun(xài ???)",
+    Callback = function()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/GoldenHeads2/f66279000c58a020e894a6db44914838/raw/62e53e1acacec0b38b43cd0f594292c32e09c39b/gistfile1.txt"))() 
+ end
+})
+AddButton(Tab1o, {
+    Name = "Kira(xài ???)",
+    Callback = function()
+ loadstring(game:HttpGet("loadstring(game:HttpGet('https://gist.githubusercontent.com/GoldenHeads2/35ca43410a2e96102f73dca904137973/raw/c4cb113194c35e0439151b06ea82dcc8053ff2c3/gistfile1.txt'))()"))()
+ end
+})
+AddButton(Tab1o, {
+    Name = "Mahito(xài ???)",
+    Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GreatestLime4K/mahitotsb/refs/heads/main/Protected_6381580361331378.txt'))()
+end
+})
+AddButton(Tab1o, {
+    Name = "Freddy(xài ???)",
+    Callback = function()
+loadstring(game:HttpGet('https://pastebin.com/raw/Ft5psDmD'))()
+end
+})
+AddButton(Tab1o, {
+    Name = "VeXor(xài ???)",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/TSB/refs/heads/main/VEXOR.lua"))() 
+end
+})
+AddButton(Tab1o, {
+    Name = "Kẻ hủy diệt(xài ???)",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/yes/refs/heads/main/Trashcan%20Man", true))()
+end
+})
+AddButton(Tab2o, {
+    Name = "Người chơi fifai(xài saitama)",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nil071n/fireman/refs/heads/main/TSB"))()
+end
+})
+
